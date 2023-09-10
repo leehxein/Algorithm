780b7aa3c7412e0ef012977cdd2300f9a7ed1ad4
leehxein <136951372+leehxein@users.noreply.github.com>
2023-09-10 15:53:36 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,6 @@
 -- 코드를 입력하세요
-SELECT ao.animal_id as ANIMAL_ID, ao.name as NAME
-from ANIMAL_INS ai right join ANIMAL_OUTS ao on ai.ANIMAL_ID = ao.ANIMAL_ID
-where ai.animal_id is null
-order by ao.animal_id
+SELECT OUTS.ANIMAL_ID, OUTS.NAME
+FROM ANIMAL_INS INS RIGHT JOIN ANIMAL_OUTS OUTS
+        ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+WHERE INS.ANIMAL_ID IS NULL 
+ORDER BY OUTS.ANIMAL_ID
