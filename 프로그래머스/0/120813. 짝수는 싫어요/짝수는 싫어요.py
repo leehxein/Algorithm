18540a698260bc4e3b4187b3f12f77d3eab76f5b
leehxein <136951372+leehxein@users.noreply.github.com>
2024-06-22 15:42:36 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,6 +1,6 @@
 def solution(n):
-    list1 = []
-    for i in range(1, n +1):
-        if i % 2 == 1:
-            list1.append(i)
-    return list1
+    answer = []
+    for i in range(1, n + 1):
+        if i % 2 != 0:
+            answer.append(i)
+    return answer
