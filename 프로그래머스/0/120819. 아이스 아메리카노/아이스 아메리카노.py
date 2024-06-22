8d6b314667fa5af38cc430877e25769806ce5e9c
leehxein <136951372+leehxein@users.noreply.github.com>
2024-06-22 16:13:53 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,6 +1,7 @@
 def solution(money):
-    coffee = 5500
-    a = money // coffee 
-    b = money % coffee
-
-    return [a, b]
+    answer = []
+    maxCup = money // 5500
+    change = money % 5500
+    answer.append(maxCup)
+    answer.append(change)
+    return answer
