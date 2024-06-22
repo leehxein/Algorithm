a7849c96edb711fe65003082babc762d2d452f32
leehxein <136951372+leehxein@users.noreply.github.com>
2024-06-22 16:03:35 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,10 +1,11 @@
 def solution(price):
+    
     if price >= 500000:
-        return int(price * 0.80)
+        answer = price * 0.8
     elif price >= 300000:
-        return int(price * 0.90)
+        answer = price * 0.9
     elif price >= 100000:
-        return int(price * 0.95)
-        
-    else: return price
-        
+        answer = price * 0.95
+    else:
+        answer = price
+    return int(answer)
